3f9bc5d8974f75ab8d2c034e263ded5e73744ee3
coalman321 <cjtucker321@gmail.com>
2022-09-14 09:28:57 -0400

    create service installer

--- launch_service/setup.py
+++ launch_service/setup.py
@@ -1,4 +1,6 @@
 from setuptools import setup
+import os
+from glob import glob
 
 package_name = 'launch_service'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob('launch/*.launch*')),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -20,7 +23,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'ros_launcher = launch_service.ros_launcher:main'
+            'ros_launcher = launch_service.ros_launcher:main',
+            'install = launch_service.install_service:main'
         ],
     },
 )
